049734a88634422b71b911087c528120fb550454
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-19 10:10:00 +0300

    Initializing the project (#1)
    * feat: init project react-vite-electron + prettier formatter
    
    * feat: add and configurate  tailwind.css
    
    * feat: add husky and lint-staged
    
    * feat: add aliases

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Test from '@components/Test';
+
+export default function App() {
+  return (
+    <>
+      <div className="text-5xl col-span-full">
+        <div>App</div>
+        <Test />
+      </div>
+    </>
+  );
+}
