d7bac9bf7573d74010e366376894742c6e71faa1
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-30 11:54:55 +0300

    refactor: rename visitor heading

--- renderer/components/VisitorForm/index.tsx
+++ renderer/components/VisitorForm/index.tsx
@@ -27,7 +27,7 @@ export default function VisitorForm() {
 
   return (
     <form className="flex flex-col gap-2" onSubmit={handleSubmit(submitForm)}>
-      <h2 className="text-3xl">Посетители</h2>
+      <h2 className="text-3xl text-center py-5">Добавить посетителя</h2>
       <Field
         id="fullname"
         register={register('fullname', { required: true })}
